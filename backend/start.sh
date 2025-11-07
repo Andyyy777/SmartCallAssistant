6f86951d4d84995f16b044dba2e273798e2b1201
Andyyy777 <liu2001qingxian@gmail.com>
2025-11-07 17:10:24 -0500

    remove cridential

--- backend/start.sh
+++ backend/start.sh
@@ -2,7 +2,7 @@
 export FLASK_APP=app.py
 export FLASK_ENV=development
 
-export OPENAI_API_KEY='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'
+export OPENAI_API_KEY=<OPENAI_API_KEY>
 # Start the Flask server
 # flask run
 python app.py
